f08e08da718cb97a6795d90b7713893955627f04
Shant Kasbarian <shantkasbarian06@gmail.com>
2024-02-03 12:53:10 +0400

    shant-kasbarian

--- src/main/java/shopping/ShoppingCart.java
+++ src/main/java/shopping/ShoppingCart.java
@@ -2,4 +2,12 @@ package shopping;
 
 public class ShoppingCart {
   // List of products, add, delete, get total price of products, display the cart
+  Product product;
+  int productId; 
+  int price;
+  public ShoppingCart() {
+    this.product = product;
+
+  }
+  
 }
