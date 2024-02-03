0861417bf6a209923065674c9e18003145445b65
Narek Nuroyan <nnareka24@gmail.com>
2024-02-03 12:59:19 +0400

    Commit by Narek

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
 import shopping.Customer;
+import shopping.Product;
 import shopping.ShoppingCart;
+
+import java.util.Scanner;
 
 /*
 Implement all necessary classes and methods with corresponding logic, and cover them with uni tests.
@@ -9,9 +12,17 @@ Implement all necessary classes and methods with corresponding logic, and cover 
 public class Main {
 
     public static void main(String[] args) {
-//        Customer customer = initCustomer();
-//        startShopping();
-//        checkout(customer);
+        Scanner scanner = new Scanner(System.in);
+        productAdding();
+        System.out.println("Enter your name -> ");
+        String customerName = scanner.nextLine();
+        System.out.println("Enter your phone number -> ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Enter your email -> ");
+        String email = scanner.nextLine();
+        Customer customer = initCustomer(customerName, phoneNumber, email);
+        startShopping();
+        checkout(customer);
 
     }
 
@@ -19,13 +30,48 @@ public class Main {
         //Prepare order to ship
     }
 
-    private static Customer initCustomer() {
+    private static Customer initCustomer(String customerName, String phoneNumber, String email) {
         // create customer
+        Customer customer = new Customer(customerName, phoneNumber, email);
+        return customer;
     }
 
-    public static void startShopping(ShoppingCart cart) {
+    public static void startShopping() {
         //start of order creation
+        ShoppingCart cart = new ShoppingCart();
+        Scanner input = new Scanner(System.in);
+        System.out.println(Product.products);
+        System.out.println("To add a product to your cart, input 1");
+        System.out.println("To delete a product from your cart, input 2");
+        System.out.println("To display the products in your cart, input 3");
+        System.out.println("To see the total price of the products you are buying, input 4");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1 -> {
+                System.out.println("Insert the product name");
+                String productName = input.nextLine();
+                cart.addToTheCart(productName);
+            }
+            case 2 -> {
+                System.out.println("Insert the name of the product you want to delete -> ");
+                String productName1 = input.nextLine();
+                cart.deleteFromTheCart(productName1);
+            }
+            case 3 -> cart.displayTheCart();
+            case 4 -> cart.totalPriceOfProducts();
+            default -> System.out.println("Please insert a number mentioned above");
+        }
+
     }
 
+    public static void productAdding() {
+        Product product = new Product();
+        product.addToTheProducts("Coca Cola - 5$");
+        product.addToTheProducts("Fanta - 5$");
+        product.addToTheProducts("Lay's - 10$");
+        product.addToTheProducts("merci(small) - 3$");
+        product.addToTheProducts("merci(medium) - 6$");
+        product.addToTheProducts("merci(big) - 10$");
+    }
 
 }
--- src/main/java/shopping/Customer.java
+++ src/main/java/shopping/Customer.java
@@ -4,5 +4,41 @@ public class Customer {
     // Customer to be able to shop each customer has its shopping Cart
     // create all necessary fields
     // make validation for fields phone number, email,
+    String customerName;
+    String phoneNumber;
+    String email;
+
+    public Customer(String customerName, String phoneNumber, String email) {
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            try {
+                if (phoneNumber.charAt(i) < 43 || (phoneNumber.charAt(i) > 43 && phoneNumber.charAt(i) < 48) || phoneNumber.charAt(i) > 57) {
+                    throw new InvalidDataException("Your phone number must contain only digits and/or a +");
+                }
+            } catch (InvalidDataException error1) {
+                System.out.println(error1);
+            }
+        }
+        try {
+            if (phoneNumber.length() > 12) {
+                throw new InvalidDataException("Your phone number must have no more than 12 digits!");
+            }
+        } catch (InvalidDataException error2) {
+            System.out.println(error2);
+        }
+
+        try {
+            if (!(email.contains("@")) && email.contains(".")) {
+                throw new InvalidDataException("Your inputted email doesn't meet the requirements");
+            }
+        } catch (InvalidDataException error3) {
+            System.out.println(error3);
+        }
+        this.customerName = customerName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+
+    }
+
+
 }
 
