0c58019c2012601d9bf6b5ad3a6891a756d43c32
vanazatyan <van.azatyan1@gmail.com>
2024-02-03 12:51:50 +0400

    A

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,28 +3,35 @@ package main;
 import shopping.Customer;
 import shopping.ShoppingCart;
 
+import java.util.LinkedList;
+
 /*
 Implement all necessary classes and methods with corresponding logic, and cover them with uni tests.
  */
 public class Main {
 
     public static void main(String[] args) {
-//        Customer customer = initCustomer();
-//        startShopping();
-//        checkout(customer);
+        Customer customer = initCustomer("gagulik", "gagulik@gmail.com");
+        startShopping(customer);
+        checkout(customer);
 
     }
+
+
 
     private static void checkout(Customer customer) {
-        //Prepare order to ship
+
     }
 
-    private static Customer initCustomer() {
-        // create customer
+    private static Customer initCustomer(String name, String email) {
+        new Customer(name, email);
+        return null;
     }
 
-    public static void startShopping(ShoppingCart cart) {
+    public static void startShopping(Customer cart) {
         //start of order creation
+        LinkedList<Customer> products = new LinkedList<>();
+        products.add(cart);
     }
 
 
