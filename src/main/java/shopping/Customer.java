1f3a8f1c43535fa1a93be0eb04a636fb324b7eaf
vanazatyan <van.azatyan1@gmail.com>
2024-02-03 13:02:08 +0400

    message

--- src/main/java/shopping/Customer.java
+++ src/main/java/shopping/Customer.java
@@ -1,6 +1,8 @@
 package shopping;
 
 public class Customer {
+    public Customer(String name, String email) {
+    }
     // Customer to be able to shop each customer has its shopping Cart
     // create all necessary fields
     // make validation for fields phone number, email,
